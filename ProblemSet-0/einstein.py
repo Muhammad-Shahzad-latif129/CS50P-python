cbce27cf6a0d9d0852b49d69e851a00a46c7e6ba
Muhammad Shahzad latif <shahzad.aya4@gmail.com>
2025-08-14 23:46:30 +0500

    Formula E = mc^2
    In this we use 
    1: Formula E = mc^2
    2: The pow() function is used to calculate powers. It takes two parameters: pow(base, exponent). For example, pow(c, 2) calculates c raised to the power of 2.

--- ProblemSet-0/einstein.py
+++ ProblemSet-0/einstein.py
@@ -3,6 +3,6 @@ def main():
     m = int(input("m: "))  #Mass input from the user
     E = m * pow(c, 2)  #Formula E = mc^2
 
-    print(f"E: {E}")  #print E
+    print(f"E: {E}")  #print Energy value
 
 main()    
