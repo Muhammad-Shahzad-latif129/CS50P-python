2f6eff5b61a89bade381af24513197c8a78b4a2e
Muhammad Shahzad latif <shahzad.aya4@gmail.com>
2025-08-14 23:43:47 +0500

    Formula E = mc^2
    In this we use 
    pow() function to take the power of the c pow() have the parameters (base, exponent) we use (c, 2) to make c to the power of 2

---
+++ ProblemSet-0/einstein.py
@@ -0,0 +1,8 @@
+def main():
+    c = 300000000  #Speed of light m/s
+    m = int(input("m: "))  #Mass input from the user
+    E = m * pow(c, 2)  #Formula E = mc^2
+
+    print(f"E: {E}")  #print E
+
+main()    
